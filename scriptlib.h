2de8a5ce242c0ddaadcc3cf76f0c395df796b925
VanillaSalt <vanillasalt5197@gmail.com>
2015-12-17 16:21:49 +0900

    fix

--- scriptlib.h
+++ scriptlib.h
@@ -21,6 +21,7 @@ public:
 	static int32 card_get_code(lua_State *L);
 	static int32 card_get_origin_code(lua_State *L);
 	static int32 card_get_origin_code_rule(lua_State *L);
+	static int32 card_is_fusion_code(lua_State *L);
 	static int32 card_is_set_card(lua_State *L);
 	static int32 card_is_pre_set_card(lua_State *L);
 	static int32 card_get_type(lua_State *L);
