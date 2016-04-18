bd7d5021cf9161021d2e4305d5e083167dd7b14b
salix5 <salix5@livemail.tw>
2016-04-18 17:31:30 +0800

    revert Card.RegisterEffect()

--- libcard.cpp
+++ libcard.cpp
@@ -981,15 +981,8 @@ int32 scriptlib::card_register_effect(lua_State *L) {
 	int32 id;
 	if (peffect->handler)
 		id = -1;
-	else {
-		if((peffect->type & (EFFECT_TYPE_TRIGGER_O | EFFECT_TYPE_TRIGGER_F)) 
-				&& !(peffect->code & 0x10032000) && (peffect->code & EVENT_PHASE)
-				&& !peffect->is_flag(EFFECT_FLAG_COUNT_LIMIT | EFFECT_FLAG_PHASE_MULTIPLE)) {
-			peffect->flag[0] |= EFFECT_FLAG_COUNT_LIMIT;
-			peffect->reset_count |= ((1 << 12) & 0xf000) | ((1 << 8) & 0xf00);
-		}
+	else
 		id = pcard->add_effect(peffect);
-	}
 	lua_pushinteger(L, id);
 	return 1;
 }
