222bfbf2d0c26b798ca4a10cd8d16b55d5a172d9
DailyShana <p2357111317192329@gmail.com>
2016-04-20 15:55:05 +0800

    revert & fix

--- effect.cpp
+++ effect.cpp
@@ -235,12 +235,13 @@ int32 effect::is_activateable(uint8 playerid, const tevent& e, int32 neglect_con
 			if((handler->data.type & TYPE_MONSTER) && (handler->current.location & LOCATION_SZONE)
 					&& !in_range(handler->current.location, handler->current.sequence))
 				return FALSE;
-			// effects with EFFECT_FLAG_SET_AVAILABLE can be activated while face-down
-			if((handler->current.location & (LOCATION_ONFIELD | LOCATION_REMOVED))
-					&& !is_flag(EFFECT_FLAG_SET_AVAILABLE)
-					&& (code != EVENT_FLIP || !(e.event_value & (FLIP_SET_AVAILABLE >> 16)))
-					&& !handler->is_status(STATUS_EFFECT_ENABLED))
-				return FALSE;
+			if((handler->current.location & (LOCATION_ONFIELD | LOCATION_REMOVED))) {
+				// effects with EFFECT_FLAG_SET_AVAILABLE can be activated while face-down
+				if(!handler->is_position(POS_FACEUP) && !is_flag(EFFECT_FLAG_SET_AVAILABLE) && (code != EVENT_FLIP || !(e.event_value & (FLIP_SET_AVAILABLE >> 16))))
+					return FALSE;
+				if(handler->is_position(POS_FACEUP) && !handler->is_status(STATUS_EFFECT_ENABLED))
+					return FALSE;
+			}
 			if(!(type & (EFFECT_TYPE_FLIP | EFFECT_TYPE_TRIGGER_F)) 
 					&& !((type & EFFECT_TYPE_SINGLE) && (code == EVENT_TO_GRAVE || code == EVENT_DESTROYED || code == EVENT_SPSUMMON_SUCCESS || code == EVENT_TO_HAND))) {
 				if((code < 1132 || code > 1149) && pduel->game_field->infos.phase == PHASE_DAMAGE && !is_flag(EFFECT_FLAG_DAMAGE_STEP))
@@ -260,7 +261,7 @@ int32 effect::is_activateable(uint8 playerid, const tevent& e, int32 neglect_con
 			if(!is_flag(EFFECT_FLAG_AVAILABLE_BD) && (type & EFFECT_TYPE_FIELD) && handler->is_status(STATUS_BATTLE_DESTROYED))
 				return FALSE;
 			if(((type & EFFECT_TYPE_FIELD) || ((type & EFFECT_TYPE_SINGLE) && is_flag(EFFECT_FLAG_SINGLE_RANGE))) && (handler->current.location & LOCATION_ONFIELD)
-			        && !handler->is_status(STATUS_EFFECT_ENABLED))
+			        && (!handler->is_position(POS_FACEUP) || !handler->is_status(STATUS_EFFECT_ENABLED)))
 				return FALSE;
 			if((type & EFFECT_TYPE_SINGLE) && is_flag(EFFECT_FLAG_SINGLE_RANGE) && !in_range(handler->current.location, handler->current.sequence))
 				return FALSE;
