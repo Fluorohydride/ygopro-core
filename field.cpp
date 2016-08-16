4388a58e3836a6aec42addf0a74b5440e084b19c
edo9300 <edoardo762@gmail.com>
2016-08-16 16:35:21 +0200

    fix unique on field

--- field.cpp
+++ field.cpp
@@ -1491,7 +1491,7 @@ effect* field::check_unique_onfield(card* pcard, uint8 controler, uint8 location
 	for(auto iter = core.unique_cards[controler].begin(); iter != core.unique_cards[controler].end(); ++iter) {
 		card* ucard = *iter;
 		if((ucard != pcard) && ucard->is_position(POS_FACEUP) && ucard->get_status(STATUS_EFFECT_ENABLED)
-			&& (ucard->unique_code == pcard->unique_code) && (ucard->unique_location & location)
+			&& (ucard->unique_code == pcard->unique_code) && (ucard->unique_location & location) && (ucard->get_code() == ucard->unique_code)
 			&& (!(pcard->current.location & ucard->unique_location) || pcard->is_position(POS_FACEDOWN)
 				|| ((!pcard->get_status(STATUS_DISABLED | STATUS_FORBIDDEN) || !ucard->get_status(STATUS_DISABLED | STATUS_FORBIDDEN)) && ucard->unique_uid < pcard->unique_uid)))
 			return pcard->unique_effect;
