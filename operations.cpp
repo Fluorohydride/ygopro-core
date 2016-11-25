c670a7c870614425194df582ef7fd85d50095a1a
VanillaSalt <vanillasalt5197@gmail.com>
2016-11-25 21:50:48 +0900

    fix

--- operations.cpp
+++ operations.cpp
@@ -1351,7 +1351,7 @@ int32 field::summon(uint16 step, uint8 sumplayer, card * target, effect * proc, 
 		int32 res = target->filter_summon_procedure(sumplayer, &eset, ignore_count, min_tribute);
 		core.select_effects.clear();
 		core.select_options.clear();
-		if(res > 0) {
+		if(res >= 0) {
 			core.select_effects.push_back(0);
 			core.select_options.push_back(1);
 		}
@@ -1849,7 +1849,7 @@ int32 field::mset(uint16 step, uint8 setplayer, card * target, effect * proc, ui
 		int32 res = target->filter_set_procedure(setplayer, &eset, ignore_count, min_tribute);
 		core.select_effects.clear();
 		core.select_options.clear();
-		if(res > 0) {
+		if(res >= 0) {
 			core.select_effects.push_back(0);
 			core.select_options.push_back(1);
 		}
