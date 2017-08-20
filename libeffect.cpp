3b005ef407f26cfb354161348b264d89bf231e0d
DailyShana <p2357111317192329@gmail.com>
2017-08-20 11:33:21 +0800

    fix Fluorohydride/ygopro#1988

--- libeffect.cpp
+++ libeffect.cpp
@@ -538,7 +538,7 @@ int32 scriptlib::effect_is_activated(lua_State * L) {
 	check_param_count(L, 1);
 	check_param(L, PARAM_TYPE_EFFECT, 1);
 	effect* peffect = *(effect**) lua_touserdata(L, 1);
-	lua_pushboolean(L, (peffect->status & EFFECT_STATUS_ACTIVATED));
+	lua_pushboolean(L, (peffect->type & 0x7f0));
 	return 1;
 }
 
