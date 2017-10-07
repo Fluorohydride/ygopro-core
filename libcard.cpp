fae2ff7a30c525ec86cbef3c6a339a7557e8961b
edo9300 <edoardo762@gmail.com>
2017-10-07 20:21:17 +0200

    Added IsLevel/Rank/Link

--- libcard.cpp
+++ libcard.cpp
@@ -782,6 +782,39 @@ int32 scriptlib::card_is_link_type(lua_State *L) {
 		lua_pushboolean(L, 0);
 	return 1;
 }
+int32 scriptlib::card_is_level(lua_State *L) {
+	check_param_count(L, 2);
+	card* pcard = *(card**)lua_touserdata(L, 1);
+	check_param(L, PARAM_TYPE_CARD, 1);
+	uint32 level = lua_tonumberint(L, 2);
+	if (pcard->get_level() == level)
+		lua_pushboolean(L, 1);
+	else
+		lua_pushboolean(L, 0);
+	return 1;
+}
+int32 scriptlib::card_is_rank(lua_State *L) {
+	check_param_count(L, 2);
+	card* pcard = *(card**)lua_touserdata(L, 1);
+	check_param(L, PARAM_TYPE_CARD, 1);
+	uint32 rank = lua_tonumberint(L, 2);
+	if (pcard->get_rank() == rank)
+		lua_pushboolean(L, 1);
+	else
+		lua_pushboolean(L, 0);
+	return 1;
+}
+int32 scriptlib::card_is_link(lua_State *L) {
+	check_param_count(L, 2);
+	card* pcard = *(card**)lua_touserdata(L, 1);
+	check_param(L, PARAM_TYPE_CARD, 1);
+	uint32 link = lua_tonumberint(L, 2);
+	if (pcard->get_link() == link)
+		lua_pushboolean(L, 1);
+	else
+		lua_pushboolean(L, 0);
+	return 1;
+}
 int32 scriptlib::card_is_race(lua_State *L) {
 	check_param_count(L, 2);
 	check_param(L, PARAM_TYPE_CARD, 1);
--- scriptlib.h
+++ scriptlib.h
@@ -104,6 +104,9 @@ public:
 	static int32 card_is_synchro_type(lua_State *L);
 	static int32 card_is_xyz_type(lua_State *L);
 	static int32 card_is_link_type(lua_State *L);
+	static int32 card_is_level(lua_State *L);
+	static int32 card_is_rank(lua_State *L);
+	static int32 card_is_link(lua_State *L);
 	static int32 card_is_race(lua_State *L);
 	static int32 card_is_attribute(lua_State *L);
 	static int32 card_is_fusion_attribute(lua_State *L);
