1126b66b8b1568bab1552627948612a23f07c678
youruriel <lurki4@web.de>
2017-10-10 17:16:49 +0200

    Fix
    removed unfortunate doubling

--- card.h
+++ card.h
@@ -217,7 +217,6 @@ public:
 	uint32 get_lscale();
 	uint32 get_rscale();
 	uint32 get_link_marker();
-	uint32 get_link_marker();
 	int32 is_link_marker(uint32 dir);
 	uint32 get_linked_zone();
 	void get_linked_cards(card_set* cset);
