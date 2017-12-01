a2a82d8314cb41e6d3b72db7d492afca72f53a62
DailyShana <p2357111317192329@gmail.com>
2017-12-01 13:26:24 +0800

    fix

--- playerop.cpp
+++ playerop.cpp
@@ -328,7 +328,9 @@ int32 field::select_chain(uint16 step, uint8 playerid, uint8 spe_count, uint8 fo
 		}
 		return FALSE;
 	} else {
-		if((returns.ivalue[0] < 0 && forced) || returns.ivalue[0] >= (int32)core.select_chains.size()) {
+		if(!forced && returns.ivalue[0] == -1)
+			return TRUE;
+		if(returns.ivalue[0] < 0 || returns.ivalue[0] >= (int32)core.select_chains.size()) {
 			pduel->write_buffer8(MSG_RETRY);
 			return FALSE;
 		}
@@ -684,7 +686,7 @@ int32 field::sort_card(int16 step, uint8 playerid, uint8 is_chain) {
 	if(step == 0) {
 		returns.bvalue[0] = 0;
 		if((playerid == 1) && (core.duel_options & DUEL_SIMPLE_AI)) {
-			returns.ivalue[0] = -1;
+			returns.bvalue[0] = -1;
 			return TRUE;
 		}
 		if(core.select_cards.empty())
