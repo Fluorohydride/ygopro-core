930564007454c3a8282b5b13bd4ff6772e4d362e
nekrozar <nekrozar@users.noreply.github.com>
2018-05-21 13:48:56 +0900

    update Card.IsLevel/Rank/Link (#163)
    * update
    
    * update

--- libcard.cpp
+++ libcard.cpp
@@ -893,33 +893,57 @@ int32 scriptlib::card_is_level(lua_State *L) {
 	check_param_count(L, 2);
 	check_param(L, PARAM_TYPE_CARD, 1);
 	card* pcard = *(card**) lua_touserdata(L, 1);
-	uint32 tlevel = lua_tointeger(L, 2);
-	if(pcard->get_level() == tlevel)
-		lua_pushboolean(L, 1);
-	else
-		lua_pushboolean(L, 0);
+	uint32 level = pcard->get_level();
+	uint32 count = lua_gettop(L) - 1;
+	uint32 result = FALSE;
+	for(uint32 i = 0; i < count; ++i) {
+		if(lua_isnil(L, i + 2))
+			continue;
+		uint32 tlevel = lua_tointeger(L, i + 2);
+		if(level == tlevel) {
+			result = TRUE;
+			break;
+		}
+	}
+	lua_pushboolean(L, result);
 	return 1;
 }
 int32 scriptlib::card_is_rank(lua_State *L) {
 	check_param_count(L, 2);
 	check_param(L, PARAM_TYPE_CARD, 1);
 	card* pcard = *(card**) lua_touserdata(L, 1);
-	uint32 trank = lua_tointeger(L, 2);
-	if(pcard->get_rank() == trank)
-		lua_pushboolean(L, 1);
-	else
-		lua_pushboolean(L, 0);
+	uint32 rank = pcard->get_rank();
+	uint32 count = lua_gettop(L) - 1;
+	uint32 result = FALSE;
+	for(uint32 i = 0; i < count; ++i) {
+		if(lua_isnil(L, i + 2))
+			continue;
+		uint32 trank = lua_tointeger(L, i + 2);
+		if(rank == trank) {
+			result = TRUE;
+			break;
+		}
+	}
+	lua_pushboolean(L, result);
 	return 1;
 }
 int32 scriptlib::card_is_link(lua_State *L) {
 	check_param_count(L, 2);
 	check_param(L, PARAM_TYPE_CARD, 1);
 	card* pcard = *(card**) lua_touserdata(L, 1);
-	uint32 tlink = lua_tointeger(L, 2);
-	if(pcard->get_link() == tlink)
-		lua_pushboolean(L, 1);
-	else
-		lua_pushboolean(L, 0);
+	uint32 link = pcard->get_link();
+	uint32 count = lua_gettop(L) - 1;
+	uint32 result = FALSE;
+	for(uint32 i = 0; i < count; ++i) {
+		if(lua_isnil(L, i + 2))
+			continue;
+		uint32 tlink = lua_tointeger(L, i + 2);
+		if(link == tlink) {
+			result = TRUE;
+			break;
+		}
+	}
+	lua_pushboolean(L, result);
 	return 1;
 }
 int32 scriptlib::card_is_race(lua_State *L) {
