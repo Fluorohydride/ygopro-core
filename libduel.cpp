5ae805f66d37dbf427fb2386dad7bd8aeddbedfd
mercury233 <me@mercury233.me>
2018-06-25 22:19:01 +0800

    fix

--- libduel.cpp
+++ libduel.cpp
@@ -3354,7 +3354,7 @@ int32 scriptlib::duel_set_dice_result(lua_State * L) {
 	int32 res;
 	for(int32 i = 0; i < 5; ++i) {
 		res = lua_tointeger(L, i + 1);
-		if(res < 1 || res > 6)
+		if(res < 1 || res > 255)
 			res = 1;
 		pduel->game_field->core.dice_result[i] = res;
 	}
