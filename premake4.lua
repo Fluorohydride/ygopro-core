f854afd8435869628a28080335831b60e0e7b61f
DailyShana <p2357111317192329@gmail.com>
2018-09-17 21:55:42 +0800

    update premake4.lua

--- premake4.lua
+++ premake4.lua
@@ -5,6 +5,6 @@ project "ocgcore"
     configuration "windows"
         includedirs { ".." }
     configuration "not vs*"
-        buildoptions { "-std=gnu++0x" }
+        buildoptions { "-std=c++14" }
     configuration "not windows"
         includedirs { "/usr/include/lua", "/usr/include/lua5.3", "/usr/include/lua/5.3" }
