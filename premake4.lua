dc099a618c56f09d3c1acc22218c250bc538b9fa
DailyShana <p2357111317192329@gmail.com>
2018-10-01 18:54:39 +0800

    update lua dependence

--- premake4.lua
+++ premake4.lua
@@ -2,9 +2,6 @@ project "ocgcore"
     kind "StaticLib"
 
     files { "**.cc", "**.cpp", "**.c", "**.h" }
-    configuration "windows"
-        includedirs { ".." }
+    includedirs { "../lua" }
     configuration "not vs*"
         buildoptions { "-std=c++14" }
-    configuration "not windows"
-        includedirs { "/usr/include/lua", "/usr/include/lua5.3", "/usr/include/lua/5.3" }
