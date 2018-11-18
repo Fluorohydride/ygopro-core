ac3cc6a0fe32f300e91681e72fd4691b8292c7db
DailyShana <p2357111317192329@gmail.com>
2018-11-18 10:03:12 +0800

    fix

--- effect.cpp
+++ effect.cpp
@@ -206,10 +206,6 @@ int32 effect::is_activateable(uint8 playerid, const tevent& e, int32 neglect_con
 					return FALSE;
 				if(handler->equiping_target)
 					return FALSE;
-				if(handler->get_status(STATUS_SET_TURN)) {
-					if((handler->data.type & TYPE_SPELL) && (handler->data.type & TYPE_QUICKPLAY))
-						return FALSE;
-				}
 				if(!(handler->data.type & (TYPE_FIELD | TYPE_PENDULUM)) && is_flag(EFFECT_FLAG_LIMIT_ZONE) && !(zone & (1u << handler->current.sequence)))
 					return FALSE;
 			} else {
