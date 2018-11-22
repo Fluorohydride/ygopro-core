689a68484db51b054e8e85ce179797b411600998
mercury233 <me@mercury233.me>
2018-11-22 22:07:17 +0800

    fix

--- libduel.cpp
+++ libduel.cpp
@@ -1115,7 +1115,7 @@ int32 scriptlib::duel_damage(lua_State *L) {
 	uint32 playerid = lua_tointeger(L, 1);
 	if(playerid != 0 && playerid != 1)
 		return 0;
-	int32 amount = lua_tointeger(L, 2);
+	int32 amount = std::round(lua_tonumber(L, 2));
 	if(amount < 0)
 		amount = 0;
 	uint32 reason = lua_tointeger(L, 3);
@@ -1133,7 +1133,7 @@ int32 scriptlib::duel_recover(lua_State *L) {
 	uint32 playerid = lua_tointeger(L, 1);
 	if(playerid != 0 && playerid != 1)
 		return 0;
-	int32 amount = lua_tointeger(L, 2);
+	int32 amount = std::round(lua_tonumber(L, 2));
 	if(amount < 0)
 		amount = 0;
 	uint32 reason = lua_tointeger(L, 3);
