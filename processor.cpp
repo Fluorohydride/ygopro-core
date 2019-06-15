a0c052357d60f8a6dcc0f6bd9d109174295bcc95
DailyShana <p2357111317192329@gmail.com>
2019-06-15 17:26:02 +0800

    fix special summon count of...
    ...effects that do not target cards

--- processor.cpp
+++ processor.cpp
@@ -4359,7 +4359,10 @@ int32 field::add_chain(uint16 step) {
 						}
 					} else {
 						uint32 sumplayer = clit.triggering_player;
-						if(optarget.op_player == 1)
+						// genarally setting op_player is unnecessary when the effect targets cards
+						// in the case of CATEGORY_SPECIAL_SUMMON(with EFFECT_FLAG_CARD_TARGET), op_player=1
+						// indecates that it is the opponent that special summons the target monsters
+						if(peffect->is_flag(EFFECT_FLAG_CARD_TARGET) && optarget.op_player == 1)
 							sumplayer = 1 - sumplayer;
 						for(auto& pcard : optarget.op_cards->container) {
 							if(pcard->spsummon_code) {
@@ -4608,7 +4611,10 @@ int32 field::solve_chain(uint16 step, uint32 chainend_arg1, uint32 chainend_arg2
 						check_card_counter(*opit, 3, 1 - sumplayer);
 					} else {
 						uint32 sumplayer = cait->triggering_player;
-						if(optarget.op_player == 1)
+						// genarally setting op_player is unnecessary when the effect targets cards
+						// in the case of CATEGORY_SPECIAL_SUMMON(with EFFECT_FLAG_CARD_TARGET), op_player=1
+						// indecates that it is the opponent that special summons the target monsters
+						if(cait->triggering_effect->is_flag(EFFECT_FLAG_CARD_TARGET) && optarget.op_player == 1)
 							sumplayer = 1 - sumplayer;
 						for(auto& ptarget : optarget.op_cards->container) {
 							if((core.global_flag & GLOBALFLAG_SPSUMMON_ONCE) && ptarget->spsummon_code)
