46e8c9269e5c32ea2e35695d1b2eaa9388dc0a77
nekrozar <nekrozar.s@gmail.com>
2019-12-18 08:56:59 +0900

    fix for Red Reboot

--- processor.cpp
+++ processor.cpp
@@ -3768,10 +3768,15 @@ int32 field::add_chain(uint16 step) {
 				for(int32 i = 0; i < eset.size(); ++i) {
 					if(eset[i]->check_count_limit(clit.triggering_player)) {
 						if(eset[i]->cost) {
-							pduel->lua->add_param(eset[i], PARAM_TYPE_EFFECT);
-							pduel->lua->add_param(phandler, PARAM_TYPE_CARD);
-							pduel->lua->add_param(clit.triggering_player, PARAM_TYPE_INT);
-							if(pduel->lua->check_condition(eset[i]->cost, 3)) {
+							if(eset[i]->cost) {
+								pduel->lua->add_param(eset[i], PARAM_TYPE_EFFECT);
+								pduel->lua->add_param(phandler, PARAM_TYPE_CARD);
+								pduel->lua->add_param(clit.triggering_player, PARAM_TYPE_INT);
+								if(pduel->lua->check_condition(eset[i]->cost, 3)) {
+									core.select_effects.push_back(eset[i]);
+									core.select_options.push_back(eset[i]->description);
+								}
+							} else {
 								core.select_effects.push_back(eset[i]);
 								core.select_options.push_back(eset[i]->description);
 							}
@@ -3802,6 +3807,7 @@ int32 field::add_chain(uint16 step) {
 		auto& clit = core.new_chains.front();
 		if(effect* peffect = core.select_effects[returns.ivalue[0]]) {
 			if(peffect->operation) {
+				peffect->type = clit.triggering_effect->type;
 				core.sub_solving_event.push_back(clit.evt);
 				add_process(PROCESSOR_EXECUTE_OPERATION, 0, peffect, 0, clit.triggering_player, 0);
 			}
