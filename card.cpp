d516f6682f0c5cf67dbfc47d0225374c8cc580ef
Mercury233 <me@mercury233.me>
2020-01-22 09:44:29 +0800

    fix refresh control status (#281)

--- card.cpp
+++ card.cpp
@@ -1975,12 +1975,14 @@ uint8 card::refresh_control_status() {
 		effect* peffect = eset.get_last();
 		if(peffect->id >= last_id) {
 			card* pcard = peffect->get_handler();
-			pduel->game_field->core.readjust_map[pcard]++;
-			if(pduel->game_field->core.readjust_map[pcard] > 3) {
+			uint8 val = (uint8)peffect->get_value(this);
+			if(val != current.controler)
+				pduel->game_field->core.readjust_map[pcard]++;
+			if(pduel->game_field->core.readjust_map[pcard] > 5) {
 				pduel->game_field->send_to(pcard, 0, REASON_RULE, peffect->get_handler_player(), PLAYER_NONE, LOCATION_GRAVE, 0, POS_FACEUP);
 				return final;
 			}
-			final = (uint8)peffect->get_value(this);
+			final = val;
 		}
 	}
 	return final;
