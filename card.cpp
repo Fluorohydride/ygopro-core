fa41db16cd69a89c9a855ba94a74995a7000def2
DailyShana <p2357111317192329@gmail.com>
2020-01-31 13:09:51 +0800

    compatibility to c++14
    conditionally-explicit constructor of std::tuple is a c++17 library feature

--- card.cpp
+++ card.cpp
@@ -1968,7 +1968,7 @@ std::tuple<uint8, effect*> card::refresh_control_status() {
 			ceffect = peffect;
 		}
 	}
-	return { final, ceffect };
+	return std::make_tuple(final, ceffect);
 }
 void card::count_turn(uint16 ct) {
 	turn_counter = ct;
