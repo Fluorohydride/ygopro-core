4fc4fb918e1242e937b3a8632e58e970391f9b64
mercury233 <me@mercury233.me>
2020-11-25 11:50:24 +0800

    init group.it
    prevent crash when calling GetNext without GetFirst

--- group.cpp
+++ group.cpp
@@ -13,15 +13,18 @@ group::group(duel* pd) {
 	ref_handle = 0;
 	pduel = pd;
 	is_readonly = FALSE;
+	it = container.begin();
 }
 group::group(duel* pd, card* pcard) {
 	container.insert(pcard);
 	ref_handle = 0;
 	pduel = pd;
 	is_readonly = FALSE;
+	it = container.begin();
 }
 group::group(duel* pd, const card_set& cset): container(cset) {
 	ref_handle = 0;
 	pduel = pd;
 	is_readonly = FALSE;
+	it = container.begin();
 }
