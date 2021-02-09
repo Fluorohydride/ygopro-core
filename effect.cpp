048c4e6bdce670d9673a396473f5bd306478a794
Mercury233 <me@mercury233.me>
2021-02-09 20:51:53 +0800

    fix RESET_SELF_TURN RESET_OPPO_TURN (#356)

--- effect.cpp
+++ effect.cpp
@@ -580,7 +580,7 @@ int32 effect::reset(uint32 reset_level, uint32 reset_type) {
 	case RESET_PHASE: {
 		if(!(reset_flag & RESET_PHASE))
 			return FALSE;
-		uint8 pid = get_handler_player();
+		uint8 pid = get_owner_player();
 		uint8 tp = handler->pduel->game_field->infos.turn_player;
 		if((((reset_flag & RESET_SELF_TURN) && pid == tp) || ((reset_flag & RESET_OPPO_TURN) && pid != tp)) 
 				&& (reset_level & 0x3ff & reset_flag))
