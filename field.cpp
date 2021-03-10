c9d639277050a82435149d4d85cf64cb0bd078d7
salix5 <salix5@livemail.tw>
2021-03-11 01:32:41 +0800

    format

--- field.cpp
+++ field.cpp
@@ -328,7 +328,8 @@ void field::move_card(uint8 playerid, card* pcard, uint8 location, uint8 sequenc
 					return;
 				} else
 					remove_card(pcard);
-			} else if(location & LOCATION_ONFIELD) {
+			}
+			else if(location & LOCATION_ONFIELD) {
 				if (playerid == preplayer && sequence == presequence)
 					return;
 				if(location == LOCATION_MZONE) {
@@ -372,11 +373,13 @@ void field::move_card(uint8 playerid, card* pcard, uint8 location, uint8 sequenc
 						pcard->unique_fieldid = UINT_MAX;
 				}
 				return;
-			} else if(location == LOCATION_HAND) {
+			}
+			else if(location == LOCATION_HAND) {
 				if(preplayer == playerid)
 					return;
 				remove_card(pcard);
-			} else {
+			}
+			else {
 				if(location == LOCATION_GRAVE) {
 					if(pcard->current.sequence == player[pcard->current.controler].list_grave.size() - 1)
 						return;
@@ -411,7 +414,8 @@ void field::move_card(uint8 playerid, card* pcard, uint8 location, uint8 sequenc
 				}
 				return;
 			}
-		} else {
+		}
+		else {
 			if((pcard->data.type & TYPE_PENDULUM) && (location == LOCATION_GRAVE)
 			        && pcard->is_capable_send_to_extra(playerid)
 			        && (((pcard->current.location == LOCATION_MZONE) && !pcard->is_status(STATUS_SUMMON_DISABLED))
