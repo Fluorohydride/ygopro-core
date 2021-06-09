6c5146016e82e9d86f16df328594f95e78414e34
mercury233 <me@mercury233.me>
2021-06-09 09:32:52 +0800

    fix

--- playerop.cpp
+++ playerop.cpp
@@ -438,16 +438,16 @@ int32 field::select_place(uint16 step, uint8 playerid, uint32 flag, uint8 count)
 			uint8 l = returns.bvalue[pt + 1];
 			uint8 s = returns.bvalue[pt + 2];
 			int32 sel = 0x1u << (s + (p == playerid ? 0 : 16) + (l == LOCATION_MZONE ? 0 : 8));
-			if(sel & (0x1 << 5))
-				sel |= 0x1 << (16 + 6);
-			if(sel & (0x1 << 6))
-				sel |= 0x1 << (16 + 5);
 			if((p != 0 && p != 1)
 					|| ((l != LOCATION_MZONE) && (l != LOCATION_SZONE))
 					|| (sel & flag) || (sel & selected)) {
 				pduel->write_buffer8(MSG_RETRY);
 				return FALSE;
 			}
+			if(sel & (0x1 << 5))
+				sel |= 0x1 << (16 + 6);
+			if(sel & (0x1 << 6))
+				sel |= 0x1 << (16 + 5);
 			selected |= sel;
 			pt += 3;
 		}
