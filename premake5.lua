8201378f6192ccb631afdb15e54f09849eb850d8
mercury233 <me@mercury233.me>
2021-11-27 17:05:54 +0800

    fix

--- premake5.lua
+++ premake5.lua
@@ -2,9 +2,9 @@ project "ocgcore"
     kind "StaticLib"
 
     files { "*.cpp", "*.h" }
-    filter "not action:vs*"
-        buildoptions { "-std=c++14" }
     if BUILD_LUA then
         links { "lua" }
         includedirs { "../lua" }
     end
+    filter "not action:vs*"
+        buildoptions { "-std=c++14" }
