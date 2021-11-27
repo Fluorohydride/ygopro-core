7aa2beda1c0918bf9756cd44a2b7b5ce52fc6c67
mercury233 <me@mercury233.me>
2021-11-27 16:25:23 +0800

    update

--- premake5.lua
+++ premake5.lua
@@ -2,9 +2,9 @@ project "ocgcore"
     kind "StaticLib"
 
     files { "*.cpp", "*.h" }
-    filter "system:windows"
-        includedirs { "../lua" }
     filter "not action:vs*"
         buildoptions { "-std=c++14" }
-    filter "not system:windows"
-        includedirs { "/usr/include/lua5.3" }
+    if BUILD_LUA then
+        links { "lua" }
+        includedirs { "../lua" }
+    end
