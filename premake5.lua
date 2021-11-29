2ee1e7358a41b86e87230751cba48cf7fbe380ae
mercury233 <me@mercury233.me>
2021-11-29 11:31:12 +0800

    lua define

--- premake5.lua
+++ premake5.lua
@@ -2,9 +2,19 @@ project "ocgcore"
     kind "StaticLib"
 
     files { "*.cpp", "*.h" }
+    links { "lua" }
+    
     if BUILD_LUA then
-        links { "lua" }
         includedirs { "../lua/src" }
     end
     filter "not action:vs*"
         buildoptions { "-std=c++14" }
+
+    filter "system:bsd"
+        defines { "LUA_USE_POSIX" }
+
+    filter "system:macosx"
+        defines { "LUA_USE_MACOSX" }
+
+    filter "system:linux"
+        defines { "LUA_USE_LINUX" }
