27425f11c531b9f2b5093a209501ef20e3b204b8
mercury233 <me@mercury233.me>
2022-04-16 10:03:15 +0800

    update premake

--- premake5.lua
+++ premake5.lua
@@ -2,10 +2,13 @@ project "ocgcore"
     kind "StaticLib"
 
     files { "*.cpp", "*.h" }
-    links { "lua" }
+    links { LUA_LIB_NAME }
     
     if BUILD_LUA then
         includedirs { "../lua/src" }
+    else
+        includedirs { LUA_INCLUDE_DIR }
+        libdirs { LUA_LIB_DIR }
     end
 
     filter "not action:vs*"
