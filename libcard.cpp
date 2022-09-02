be4eb2b5af6a12126f4bb08c5cfd440b73f973b7
Aephiex <34618932+Aephiex@users.noreply.github.com>
2022-09-02 11:14:40 +0800

    Update libcard.cpp

--- libcard.cpp
+++ libcard.cpp
@@ -2882,16 +2882,11 @@ int32 scriptlib::card_is_can_be_synchro_material(lua_State *L) {
 	check_param(L, PARAM_TYPE_CARD, 1);
 	card* pcard = *(card**) lua_touserdata(L, 1);
 	card* scard = 0;
-	card* tuner = 0;
-	if(lua_gettop(L) >= 2) {
+	if(lua_gettop(L) >= 2 && !lua_isnil(L, 2)) {
 		check_param(L, PARAM_TYPE_CARD, 2);
 		scard = *(card**) lua_touserdata(L, 2);
 	}
-	if(lua_gettop(L) >= 3) {
-		check_param(L, PARAM_TYPE_CARD, 3);
-		tuner = *(card**) lua_touserdata(L, 3);
-	}
-	lua_pushboolean(L, pcard->is_can_be_synchro_material(scard, tuner));
+	lua_pushboolean(L, pcard->is_can_be_synchro_material(scard));
 	return 1;
 }
 int32 scriptlib::card_is_can_be_ritual_material(lua_State *L) {
