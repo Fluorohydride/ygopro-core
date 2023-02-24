5b1269f72c2ff3a8ee34076ddadc974d1b47e888
mercury233 <me@mercury233.me>
2023-02-24 11:37:31 +0800

    fix event after end phase

--- processor.cpp
+++ processor.cpp
@@ -3932,12 +3932,17 @@ int32 field::process_turn(uint16 step, uint8 turn_player) {
 		return FALSE;
 	}
 	case 18: {
+		if (core.new_fchain.size() || core.new_ochain.size())
+			add_process(PROCESSOR_POINT_EVENT, 0, 0, 0, 0, 0);
+		return FALSE;
+	}
+	case 19: {
 		raise_event((card*)0, EVENT_TURN_END, 0, 0, 0, turn_player, 0);
 		process_instant_event();
 		adjust_all();
 		return FALSE;
 	}
-	case 19: {
+	case 20: {
 		core.new_fchain.clear();
 		core.new_ochain.clear();
 		core.quick_f_chain.clear();
