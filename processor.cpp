18d58777081e6af16be925e1d6b49de2d65702e5
Mercury233 <me@mercury233.me>
2023-05-18 14:52:44 +0800

    format

--- processor.cpp
+++ processor.cpp
@@ -3305,7 +3305,7 @@ int32 field::process_damage_step(uint16 step, uint32 new_attack) {
 	case 3: {
 		core.attacker = (card*)core.units.begin()->peffect;
 		core.attack_target = (card*)core.units.begin()->ptarget;
-		if (core.attacker) {
+		if(core.attacker) {
 			core.attacker->set_status(STATUS_ATTACK_CANCELED, TRUE);
 			core.attacker->announce_count++;
 			core.attacker->announced_cards.addcard(core.attack_target);
