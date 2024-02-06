7ba111315533fab1c6f8d5cd444250b7d044da35
salix5 <salix5566@gmail.com>
2024-02-07 02:55:21 +0800

    equip: fix card type
    https://www.db.yugioh-card.com/yugiohdb/faq_search.action?ope=5&fid=9999&request_locale=ja

--- operations.cpp
+++ operations.cpp
@@ -1409,10 +1409,15 @@ int32 field::equip(uint16 step, uint8 equip_player, card * equip_card, card * ta
 			peffect->owner = equip_card;
 			peffect->handler = equip_card;
 			peffect->type = EFFECT_TYPE_SINGLE;
-			if(equip_card->get_type() & TYPE_TRAP) {
+			if (equip_card->data.type & TYPE_TOKEN) {
+				peffect->code = EFFECT_ADD_TYPE;
+				peffect->value = TYPE_EQUIP + TYPE_SPELL;
+			}
+			else if (equip_card->get_type() & TYPE_TRAP) {
 				peffect->code = EFFECT_ADD_TYPE;
 				peffect->value = TYPE_EQUIP;
-			} else {
+			}
+			else {
 				peffect->code = EFFECT_CHANGE_TYPE;
 				peffect->value = TYPE_EQUIP + TYPE_SPELL;
 			}
