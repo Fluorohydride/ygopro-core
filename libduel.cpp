6bf956ebdcc601c77c53e88c09ded6c1bc01d545
mercury233 <me@mercury233.me>
2024-04-04 14:01:16 +0800

    update

--- libduel.cpp
+++ libduel.cpp
@@ -3177,8 +3177,10 @@ int32 scriptlib::duel_get_synchro_material(lua_State *L) {
 	group* pgroup = pduel->new_group();
 	for (auto cit = mats.begin(); cit != mats.end(); ++cit) {
 		card* pcard = *cit;
-		if (facedown || (pcard->current.location != LOCATION_MZONE || pcard->is_position(POS_FACEUP)))
-			pgroup->container.insert(*cit);
+		if (pcard->get_status(STATUS_SUMMONING | STATUS_SUMMON_DISABLED | STATUS_SPSUMMON_STEP)
+			|| pcard->current.location == LOCATION_MZONE && !pcard->is_position(POS_FACEUP) && !facedown)
+			continue;
+		pgroup->container.insert(*cit);
 	}
 	interpreter::group2value(L, pgroup);
 	return 1;
