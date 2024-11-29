65bd7797b88b3950bbab2e5365fa778f20861292
salix5 <salix5566@gmail.com>
2024-11-29 15:18:41 +0800

    add vector_fread

--- buffer.h
+++ buffer.h
@@ -1,6 +1,7 @@
 #ifndef CORE_BUFFER_H
 #define CORE_BUFFER_H
 
+#include <cstdio>
 #include <cstring>
 #include <vector>
 
@@ -32,6 +33,13 @@ inline void vector_write_block(std::vector<unsigned char>& buffer, const void* s
 template<typename T>
 inline void vector_write(std::vector<unsigned char>& buffer, T value) {
 	vector_write_block(buffer, &value, sizeof(T));
+}
+
+inline void vector_fread(std::vector<unsigned char>& buffer, FILE* fp) {
+	unsigned char temp[4096]{};
+	while (size_t len = std::fread(temp, 1, sizeof temp, fp))
+		vector_write_block(buffer, temp, len);
+	std::fclose(fp);
 }
 
 #endif // !CORE_BUFFER_H
