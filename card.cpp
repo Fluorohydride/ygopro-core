96215054ffda0238778fcb5462da102ea4572ab2
salix5 <salix5566@gmail.com>
2025-02-14 02:27:37 +0800

    add EFFECT_ALLOW_FOR_SYNCHRO

--- card.cpp
+++ card.cpp
@@ -4077,7 +4077,7 @@ int32_t card::is_can_be_fusion_material(card* fcard, uint32_t summon_type) {
 	return TRUE;
 }
 int32_t card::is_can_be_synchro_material(card* scard, card* tuner) {
-	if ((data.type & (TYPE_XYZ | TYPE_LINK)) && !is_affected_by_effect(EFFECT_SYNCHRO_LEVEL))
+	if ((data.type & (TYPE_XYZ | TYPE_LINK)) && !is_affected_by_effect(EFFECT_ALLOW_FOR_SYNCHRO))
 		return FALSE;
 	if(!(get_synchro_type() & TYPE_MONSTER))
 		return FALSE;
