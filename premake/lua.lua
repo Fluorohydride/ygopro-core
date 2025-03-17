88f434a2ae130538444f859fcbbf354ab69802ae
salix5 <salix5566@gmail.com>
2025-03-17 19:33:28 +0800

    remove src/onelua.c

--- premake/lua.lua
+++ premake/lua.lua
@@ -3,7 +3,7 @@ project "lua"
     compileas "C++"
 
     files { "src/*.c", "src/*.h" }
-    removefiles { "src/lua.c", "src/luac.c", "src/linit.c" }
+    removefiles { "src/lua.c", "src/luac.c", "src/linit.c", "src/onelua.c" }
 
     filter "configurations:Debug"
         defines { "LUA_USE_APICHECK" }
