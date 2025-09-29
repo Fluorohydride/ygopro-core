1cf899e1a0925f8632756f5cbfada201853bbcb3
salix5 <salix5566@gmail.com>
2025-09-29 10:05:38 +0800

    disable non-text load_script (#796)
    * use text mode in luaL_loadbufferx
    
    * Add enable_unsafe_feature flag

--- interpreter.cpp
+++ interpreter.cpp
@@ -14,7 +14,8 @@
 #include "ocgapi.h"
 #include "interpreter.h"
 
-interpreter::interpreter(duel* pd, bool enable_unsafe_libraries) : coroutines(256), pduel(pd) {
+interpreter::interpreter(duel* pd, bool enable_unsafe_libraries)
+	: coroutines(256), pduel(pd), enable_unsafe_feature(enable_unsafe_libraries) {
 	lua_state = luaL_newstate();
 	current_state = lua_state;
 	std::memcpy(lua_getextraspace(lua_state), &pd, LUA_EXTRASPACE); //set_duel_info
@@ -139,7 +140,11 @@ int32_t interpreter::load_script(const char* script_name) {
 		return OPERATION_FAIL;
 	++no_action;
 	luaL_checkstack(current_state, 2, nullptr);
-	int32_t error = luaL_loadbuffer(current_state, (const char*)buffer, len, script_name) || lua_pcall(current_state, 0, 0, 0);
+	int32_t error = 0;
+	if (enable_unsafe_feature)
+		error = luaL_loadbuffer(current_state, (const char*)buffer, len, script_name) || lua_pcall(current_state, 0, 0, 0);
+	else
+		error = luaL_loadbufferx(current_state, (const char*)buffer, len, script_name, "t") || lua_pcall(current_state, 0, 0, 0);
 	if (error) {
 		interpreter::sprintf(pduel->strbuffer, "%s", lua_tostring(current_state, -1));
 		handle_message(pduel, 1);
