5e200e38aefb78aa43af0d742ebb8614cfa42884
salix5 <salix5566@gmail.com>
2026-04-06 23:21:46 +0800

    Fix write_setcode

--- card_data.h
+++ card_data.h
@@ -34,17 +34,19 @@ inline void write_setcode(uint16_t list[], uint64_t value, size_t begin = 0) {
 	if (begin % 4 || begin > SIZE_SETCODE - 4)
 		return;
 	if (!value) {
-		std::memset(list + begin, 0, sizeof(uint64_t));
+		std::memset(list + begin, 0, sizeof(uint16_t) * 4);
 		return;
 	}
-	size_t len = begin;
+	size_t index = begin;
 	for (int32_t i = 0; i < 4; ++i) {
 		uint16_t section = (value >> (16 * i)) & 0xffff;
 		if (section) {
-			list[len] = section;
-			++len;
+			list[index] = section;
+			index++;
 		}
 	}
+	if (index < begin + 4)
+		std::memset(list + index, 0, sizeof(uint16_t) * (begin + 4 - index));
 }
 
 struct card_data {
